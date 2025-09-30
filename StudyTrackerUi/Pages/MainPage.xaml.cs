4e71fc57f01b574d728374a974850e3b3e520b82
neocim <neocim@proton.me>
2025-09-30 21:59:36 +0300

    Refactoring

---
+++ StudyTrackerUi/Pages/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace StudyTrackerUi.Pages;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void RainbowOnClicked(object? sender, EventArgs e)
+    {
+        Button.Text = "Rainbow on";
+    }
+}
