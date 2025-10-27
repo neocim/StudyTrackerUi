a6d00fc84dfbc6f7d77e340a9dc8331c204b0fbd
neocim <frag1337n@gmail.com>
2025-10-27 15:16:10 +0300

    Add binding context

--- StudyTrackerUi/Pages/CreateTaskPage.xaml.cs
+++ StudyTrackerUi/Pages/CreateTaskPage.xaml.cs
@@ -1,3 +1,5 @@
+using StudyTrackerUi.Views;
+
 namespace StudyTrackerUi.Pages;
 
 public partial class CreateTaskPage : ContentPage
@@ -5,14 +7,12 @@ public partial class CreateTaskPage : ContentPage
     public CreateTaskPage()
     {
         InitializeComponent();
+        BindingContext = new CreateTaskViewModel();
     }
 
     private void CreateButtonClicked(object? sender, EventArgs e)
     {
-        CreateTaskButton.Text = "Clicked";
-    }
-
-    private void BeginDateSelected(object sender, DateChangedEventArgs e)
-    {
+        var viewModel = (CreateTaskViewModel)BindingContext;
+        CreateTaskButton.Text = $"{viewModel.Name}";
     }
 }
---
+++ StudyTrackerUi/Views/CreateTask.cs
@@ -0,0 +1,86 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace StudyTrackerUi.Views;
+
+public sealed class CreateTaskViewModel : INotifyPropertyChanged
+{
+    private string _name { get; set; }
+    private string _description { get; set; }
+    private bool _success { get; set; }
+
+    private DateOnly _beginDate { get; set; }
+    private DateOnly _endDate { get; set; }
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (_name != value)
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public string Description
+    {
+        get => _description;
+        set
+        {
+            if (_description != value)
+            {
+                _description = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public bool Success
+    {
+        get => _success;
+        set
+        {
+            if (_success != value)
+            {
+                _success = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public DateOnly BeginDate
+    {
+        get => _beginDate;
+        set
+        {
+            if (_beginDate != value)
+            {
+                _beginDate = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public DateOnly EndDate
+    {
+        get => _endDate;
+        set
+        {
+            if (_endDate != value)
+            {
+                _endDate = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string? name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
