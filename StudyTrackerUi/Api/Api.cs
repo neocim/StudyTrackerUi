aa26cffaeaad6b0ccb7609d027036a0494a1bcfa
neocim <frag1337n@gmail.com>
2025-11-09 17:21:48 +0300

    Add `GetTask` in `Api`

--- StudyTrackerUi/Api/Api.cs
+++ StudyTrackerUi/Api/Api.cs
@@ -1,11 +1,24 @@
+using System.Net;
+using ErrorOr;
+using Newtonsoft.Json;
+using TaskDto = StudyTrackerUi.Api.Dto.Task;
+
 namespace StudyTrackerUi.Api;
 
 public sealed class Api(HttpClient httpClient, ApiQuery apiQuery)
 {
-    public async Task GetTask(Guid userId, Guid taskId)
+    public async Task<ErrorOr<TaskDto>> GetTask(Guid userId, Guid taskId)
     {
         var request = apiQuery.User(userId).Task.GetOne(taskId);
         var response = await httpClient.GetAsync(request);
+
         var jsonResponse = await response.Content.ReadAsStringAsync();
+
+        if (response.StatusCode != HttpStatusCode.OK)
+            return Error.Custom(999, response.StatusCode.ToString(), jsonResponse);
+
+        var task = JsonConvert.DeserializeObject<TaskDto>(jsonResponse)!;
+
+        return task;
     }
 }
--- StudyTrackerUi/Api/Dto/TaskNode.cs
+++ StudyTrackerUi/Api/Dto/TaskNode.cs
@@ -1,9 +1,10 @@
 namespace StudyTrackerUi.Api.Dto;
 
-public record Task(
+public record TaskNode(
     Guid Id,
     Guid OwnerId,
     Guid? ParentId,
+    ICollection<TaskNode>? SubTasks,
     DateOnly BeginDate,
     DateOnly EndDate,
     string Name,
